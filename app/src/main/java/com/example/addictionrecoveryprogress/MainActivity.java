c3b9b93108d40958c4ab9bdc82ad7b2849c30045
Thomas Nichols <ta.nichols@outlook.com>
2016-10-26 17:21:06 -0700

    Changed kkingdon comment

--- app/src/main/java/com/example/addictionrecoveryprogress/MainActivity.java
+++ app/src/main/java/com/example/addictionrecoveryprogress/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-  // Teach 07 Part II: Comment #1 -- kkingdon
+  // Teach 07 Part II: Comment #1 -- kkingdon -- great comment!
   // Teach 07 Part II: Comment #2 -- tanichols
   // Teach 07 Part II: Comment #3 -- jvehikite
 
