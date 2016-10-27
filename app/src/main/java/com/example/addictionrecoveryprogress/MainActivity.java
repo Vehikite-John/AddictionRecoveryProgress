084b7aa29427c7b75ca1b96000369b8fa0a8416a
John Vehikite <jdavet1244@gmail.com>
2016-10-26 17:35:47 -0700

    Teach 07 Part III Conflict #3

--- app/src/main/java/com/example/addictionrecoveryprogress/MainActivity.java
+++ app/src/main/java/com/example/addictionrecoveryprogress/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
   // Teach 07 Part II: Comment #1 -- kkingdon
   // Teach 07 Part II: Comment #2 -- tanichols
-  // Teach 07 Part II: Comment #3 -- jvehikite
+  // Teach 07 Part II: Conflict #3 -- jvehikite
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
