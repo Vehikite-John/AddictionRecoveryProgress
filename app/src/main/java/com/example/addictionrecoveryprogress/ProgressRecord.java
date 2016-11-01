df02fc843f90184fc94a63647ed7940ec8d604d2
kin15016 <kin15016@byui.edu>
2016-10-31 20:12:27 -0700

    Completed ProgressRecord class. Completed unit tests for ProgressRecord class.

--- app/src/main/java/com/example/addictionrecoveryprogress/ProgressRecord.java
+++ app/src/main/java/com/example/addictionrecoveryprogress/ProgressRecord.java
@@ -1,15 +1,61 @@
 package com.example.addictionrecoveryprogress;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * The record of one day's addiction-recovery progress
  */
 class ProgressRecord {
-  private Date _date;
+  private Calendar _date;
+  private boolean _victory;
+  private Mood _mood;
+  private Location _location;
+  private TimePeriod _timePeriod;
 
-  ProgressRecord(Date date) {
+  enum Mood {
+    STRESSED,
+    BORED,
+    SAD,
+    ANGRY,
+    LONELY
+  }
+
+  enum Location {
+    HOME,
+    WORK,
+    TRANSPORTATION,
+    ACCOMODATION
+  }
+
+  enum TimePeriod {
+    MORNING,
+    AFTERNOON,
+    EVENING,
+    NIGHT
+  }
+
+  /**
+   * Default progress record constructor. Initializes date to yesterday.
+   */
+  ProgressRecord() {
+    this(GregorianCalendar.getInstance());
+    _date.add(Calendar.DATE, -1);
+  }
+
+  /**
+   * Constructs progress record for a specific date.
+   *
+   * @param date the date of the progress record.
+   */
+  ProgressRecord(Calendar date) {
     _date = date;
+
+    // ensure date is rounded down to start of day.
+    _date.set(Calendar.HOUR_OF_DAY, 0);
+    _date.set(Calendar.MINUTE, 0);
+    _date.set(Calendar.SECOND, 0);
+    _date.set(Calendar.MILLISECOND, 0);
   }
 
   /**
@@ -18,7 +64,7 @@ class ProgressRecord {
    * @return true if day was a victory; false if day was a setback.
    */
   boolean isVictory() {
-    return false;
+    return _victory;
   }
 
   /**
@@ -27,6 +73,70 @@ class ProgressRecord {
    * @param status true if the day was a victory; false if the day was a setback.
    */
   void setVictory(boolean status) {
+    _victory = status;
+  }
+
+  /**
+   * Retrieves the recorded mood at the time of a setback.
+   *
+   * @return mood at the time of a setback.
+   */
+  Mood getMood() {
+    return _mood;
+  }
+
+  /**
+   * Sets the mood at the time of a setback.
+   *
+   * @param mood how you were feeling at the time of a setback.
+   */
+  void setMood(Mood mood) {
+    _mood = mood;
+  }
+
+  /**
+   * Retrieves the recorded location at the time of a setback
+   *
+   * @return the location of the setback
+   */
+  Location getLocation() {
+    return _location;
+  }
+
+  /**
+   * Sets the location at the time of a setback
+   *
+   * @param location where you were at the time of a setback.
+   */
+  void setLocation(Location location) {
+    _location = location;
+  }
+
+  /**
+   * Retrieves the recorded time period of a setback.
+   *
+   * @return the time when the setback occurred.
+   */
+  TimePeriod getTimePeriod() {
+    return _timePeriod;
+  }
+
+  /**
+   * Sets the time period of a setback
+   *
+   * @param timePeriod when the setback occurred.
+   */
+  void setTimePeriod(TimePeriod timePeriod) {
+    _timePeriod = timePeriod;
+  }
+
+  /**
+   * Returns date of this record
+   *
+   * @return the date of with this progress record
+   */
+  Calendar getDate() {
+    return _date;
   }
 
 }
--- app/src/test/java/com/example/addictionrecoveryprogress/ProgressRecordUnitTest.java
+++ app/src/test/java/com/example/addictionrecoveryprogress/ProgressRecordUnitTest.java
@@ -2,7 +2,8 @@ package com.example.addictionrecoveryprogress;
 
 import org.junit.Test;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,12 +13,63 @@ import static org.junit.Assert.assertEquals;
 public class ProgressRecordUnitTest {
   @Test
   public void testSetVictory() throws Exception {
-    ProgressRecord pr = new ProgressRecord(new Date());
+    ProgressRecord pr = new ProgressRecord(new GregorianCalendar());
 
     pr.setVictory(true);
     assertEquals(pr.isVictory(), true);
 
     pr.setVictory(false);
     assertEquals(pr.isVictory(), false);
+  }
+
+  @Test
+  public void testSetMood() throws Exception {
+    ProgressRecord pr = new ProgressRecord(new GregorianCalendar());
+
+    for (ProgressRecord.Mood mood : ProgressRecord.Mood.values()) {
+      pr.setMood(mood);
+      assertEquals(pr.getMood(), mood);
+    }
+  }
+
+  @Test
+  public void testSetLocation() throws Exception {
+    ProgressRecord pr = new ProgressRecord(new GregorianCalendar());
+
+    for (ProgressRecord.Location location : ProgressRecord.Location.values()) {
+      pr.setLocation(location);
+      assertEquals(pr.getLocation(), location);
+    }
+  }
+
+  @Test
+  public void testSetTimePeriod() throws Exception {
+    ProgressRecord pr = new ProgressRecord(new GregorianCalendar());
+
+    for (ProgressRecord.TimePeriod period : ProgressRecord.TimePeriod.values()) {
+      pr.setTimePeriod(period);
+      assertEquals(pr.getTimePeriod(), period);
+    }
+  }
+
+  @Test
+  public void testGetDate() throws Exception {
+    Calendar date = new GregorianCalendar();
+    ProgressRecord pr = new ProgressRecord(date);
+
+    // Ensure that only the year, month and day are set; other fields are set to zero.
+    assertEquals(pr.getDate().get(Calendar.YEAR), date.get(Calendar.YEAR));
+    assertEquals(pr.getDate().get(Calendar.MONTH), date.get(Calendar.MONTH));
+    assertEquals(pr.getDate().get(Calendar.DAY_OF_MONTH), date.get(Calendar.DAY_OF_MONTH));
+    assertEquals(pr.getDate().get(Calendar.HOUR_OF_DAY), 0);
+    assertEquals(pr.getDate().get(Calendar.HOUR), 0);
+    assertEquals(pr.getDate().get(Calendar.MINUTE), 0);
+    assertEquals(pr.getDate().get(Calendar.SECOND), 0);
+    assertEquals(pr.getDate().get(Calendar.MILLISECOND), 0);
+
+    // verify that default constructor returns yesterday's date
+    date.add(Calendar.DATE, -1);
+    pr = new ProgressRecord();
+    assertEquals(pr.getDate().get(Calendar.DATE), date.get(Calendar.DATE));
   }
 }
