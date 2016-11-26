ae9cfc6ab8b8f5fb4bbdce74c481573149415dc4
Thomas Nichols <ta.nichols@outlook.com>
2016-11-26 10:12:09 -0800

    Updated ViewRecord Activity and layout to display statistics from RecordOperations

--- app/src/main/java/com/example/addictionrecoveryprogress/ViewRecordActivity.java
+++ app/src/main/java/com/example/addictionrecoveryprogress/ViewRecordActivity.java
@@ -1,19 +1,32 @@
 package com.example.addictionrecoveryprogress;
 
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ViewRecordActivity extends ListActivity {
+public class ViewRecordActivity extends AppCompatActivity {
 
     private RecordOperations recordOps;
-    List<ProgressRecord> records;
+    private int longestStreak;
+    private int currentStreak;
+    private int totalVictories;
+    private int totalVictoriesPercent;
+    private int monthVictories;
+    private int monthVictoriesPercent;
+
+    private TextView totalVictoriesText = null;
+    private TextView totalVictoriesPercentText = null;
+    private TextView monthVictoriesText = null;
+    private TextView monthVictoriesPercentText = null;
+    private TextView longestStreakText = null;
+    private TextView currentSreakText = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +36,45 @@ public class ViewRecordActivity extends ListActivity {
 
         try {
             recordOps.open();
-            recordOps.getRecord(-1);
-            records = recordOps.getAllRecords();
+            longestStreak = recordOps.getLongestStreak();
+            currentStreak = recordOps.getCurrentStreak();
+            totalVictories = recordOps.getTotalVictories();
+            totalVictoriesPercent = recordOps.getTotalVictoriesPercent();
+            monthVictories = recordOps.getMonthVictories();
+            monthVictoriesPercent = recordOps.getMonthVictoriesPercent();
             recordOps.close();
         } catch (Exception ex) {
             Logger.getLogger(ViewRecordActivity.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        ArrayAdapter<ProgressRecord> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, records);
-        setListAdapter(adapter);
+        LinearLayout lView = (LinearLayout)findViewById(R.id.statisticsLinearlayout);
+
+        totalVictoriesText = new TextView(this);
+        totalVictoriesText.setText("Total Victories: " + totalVictories);
+        lView.addView(totalVictoriesText);
+
+        totalVictoriesPercentText = new TextView(this);
+        totalVictoriesPercentText.setText("Total Victories Percent: " + totalVictoriesPercent);
+        lView.addView(totalVictoriesPercentText);
+
+        monthVictoriesText = new TextView(this);
+        monthVictoriesText.setText("Month Victories: " + monthVictories);
+        lView.addView(monthVictoriesText);
+
+        monthVictoriesPercentText = new TextView(this);
+        monthVictoriesPercentText.setText("Month Victories Percent: " + monthVictoriesPercent);
+        lView.addView(monthVictoriesPercentText);
+
+        longestStreakText = new TextView(this);
+        longestStreakText.setText("Longest Streak: " + longestStreak);
+        lView.addView(longestStreakText);
+
+        currentSreakText = new TextView(this);
+        currentSreakText.setText("Current Streak: " + currentStreak);
+        lView.addView(currentSreakText);
+
+
+
     }
 
     /**
